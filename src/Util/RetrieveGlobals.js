562c399f33525cad032aa40f465d4be0789d43b6
Zach Leatherman <zachleat@users.noreply.github.com>
2026-07-25 20:45:19 -0500

    Cleanup, dependency no longer necessary

--- src/Util/RetrieveGlobals.js
+++ src/Util/RetrieveGlobals.js
@@ -1,4 +1,3 @@
-import { RetrieveGlobals as NodeRetrieveGlobals } from "node-retrieve-globals";
 import { parseCode, walkCode, importFromString } from "import-module-string";
 import { isBuiltin } from "node:module";
 
@@ -14,30 +13,6 @@ export async function RetrieveGlobals(code, filePath, options = {}) {
 		},
 	};
 
-	let ast = parseCode(code);
-	let { imports } = walkCode(ast);
-
-	let nonBuiltinImports = Array.from(imports).filter((name) => !isBuiltin(name));
-	if (nonBuiltinImports.length === 0) {
-		let implicitExports = isJavaScriptFrontMatterCompat ? false : true;
-		return importFromString(code, { ast, data, filePath, implicitExports });
-	}
-
-	// TODO re-use already parsed AST from `import-module-string` in `node-retrieve-globals`
-	let vm = new NodeRetrieveGlobals(code, {
-		filePath,
-		// ignored if vm.Module is stable (or --experimental-vm-modules)
-		transformEsmImports: true,
-	});
-
-	// Future warning until vm.Module is stable:
-	// If the frontMatterCode uses `import` this uses the `experimentalModuleApi`
-	// option in node-retrieve-globals to workaround https://github.com/zachleat/node-retrieve-globals/issues/2
-
-	// this is async, but it’s handled in Eleventy upstream.
-	return vm.getGlobalContext(data, {
-		reuseGlobal: true,
-		dynamicImport: true,
-		// addRequire: true,
-	});
+	let implicitExports = isJavaScriptFrontMatterCompat ? false : true;
+	return importFromString(code, { data, filePath, implicitExports });
 }
